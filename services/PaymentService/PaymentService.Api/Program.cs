5af25aae3135537ff9bb2fa18e15c15778f3676e
nalankaraa <nalankaraa3@gmail.com>
2026-04-02 20:49:23 +0300

    migrate Availability, Payment, and Notification services to MongoDB

--- services/PaymentService/PaymentService.Api/Program.cs
+++ services/PaymentService/PaymentService.Api/Program.cs
@@ -2,13 +2,23 @@ using PaymentService.Application.Repositories;
 using PaymentService.Application.Services;
 using PaymentService.Infrastructure.Repositories;
 using BuildingBlocks.Infrastructure.Auth;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
+var mongoSettings = builder.Configuration.GetSection("Mongo");
+var mongoConnectionString = mongoSettings["ConnectionString"] ?? throw new InvalidOperationException("Mongo:ConnectionString is required.");
+var mongoDatabaseName = mongoSettings["DatabaseName"] ?? throw new InvalidOperationException("Mongo:DatabaseName is required.");
 
 builder.Services.AddControllers();
 builder.Services.AddSharedJwtAuthentication(builder.Configuration);
+builder.Services.AddSingleton<IMongoClient>(_ => new MongoClient(mongoConnectionString));
+builder.Services.AddSingleton(sp =>
+{
+    var client = sp.GetRequiredService<IMongoClient>();
+    return client.GetDatabase(mongoDatabaseName);
+});
 
-builder.Services.AddSingleton<IPaymentRepository, InMemoryPaymentRepository>();
+builder.Services.AddScoped<IPaymentRepository, MongoPaymentRepository>();
 builder.Services.AddScoped<IPaymentService, PaymentService.Application.Services.PaymentService>();
 
 var app = builder.Build();
