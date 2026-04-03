8123fa206677ef85af0bbfddb43edae71ed30350
nalankaraa <nalankaraa3@gmail.com>
2026-04-03 23:06:58 +0300

    lock reservation-availability client contract

--- services/ReservationService/ReservationService.Application/Clients/SeatLockResult.cs
+++ services/ReservationService/ReservationService.Application/Clients/SeatLockResult.cs
@@ -5,4 +5,8 @@ public class SeatLockResult
     public bool Success { get; set; }
     public bool IsConflict { get; set; }
     public bool IsServiceUnavailable { get; set; }
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
